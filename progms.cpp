8c6f90d013c4ec04f0835d1334fe37db7be11a91
Khushi Yadav <kky957242@gmail.com>
2024-12-21 23:20:40 +0530

    Create progms.cpp

---
+++ progms.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter the value of number :" ;
+cin >> n;
+if(n>0){
+cout<<"no is positive";
+}
+else if (n=0){
+cout<<"no is zero ";}
+else {
+cout<<"no is negative";
+}
+}
